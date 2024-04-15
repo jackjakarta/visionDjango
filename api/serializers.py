a8da18b4de5d8b29817c40b05ea65a6ec8ce6d60
jackjakarta <al.termure@gmail.com>
2024-04-15 18:15:36 +0200

    basic implementation for api endpoints with api key auth

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from website.models import Narration, Video
+from website.models import Narration, Video, Audio
 
 AuthUser = get_user_model()
 
@@ -9,31 +9,34 @@ AuthUser = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuthUser
-        exclude = [
-            "id",
-            'first_name',
-            'last_name',
-            "last_login",
-            "password",
-            "is_superuser",
-            "is_staff",
-            "is_active",
-            "date_joined",
-            "groups",
-            "user_permissions",
-        ]
+        fields = ['email']
+
+
+class AudioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Audio
+        exclude = []
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        exclude = []
 
 
 class NarrationSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=AuthUser.objects.all(), required=False)
+    audio = AudioSerializer()
+    video = VideoSerializer()
 
     class Meta:
         model = Narration
-        exclude = ["created_at", "updated_at", ]
+        exclude = []
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         user = instance.user
+
         if user:
             representation['user'] = UserSerializer(user).data
 
--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import NarrationViewSet
+from .viewsets import NarrationsViewSet
 
 app_name = 'api'
 
-router = routers.DefaultRouter()
-
 
 # API Routes
-router.register(r'narrations', NarrationViewSet, 'narrations')
+router = routers.DefaultRouter()
+router.register(r'narrations', NarrationsViewSet, 'narrations')
 
 
 urlpatterns = [
