754b045a4f65d90f55a59fb3f11a2c15f79dcec1
jackjakarta <al.termure@gmail.com>
2024-10-06 19:49:44 +0200

    formatting js

--- static/website/js/custom-field.js
+++ static/website/js/custom-field.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    $('#switchCheckDefault').change(function(){
-        if(this.checked) {
-            $('#promptField').show();
-        } else {
-            $('#promptField').hide();
-        }
-    });
+$(document).ready(function () {
+  $("#switchCheckDefault").change(function () {
+    if (this.checked) {
+      $("#promptField").show();
+    } else {
+      $("#promptField").hide();
+    }
+  });
 });
--- static/website/js/form-loader-basic.js
+++ static/website/js/form-loader-basic.js
@@ -1,8 +1,8 @@
-document.getElementById('myForm').addEventListener('submit', function(e) {
-    // Show the spinner
-    document.getElementById('loading-container').style.display = 'block';
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  // Show the spinner
+  document.getElementById("loading-container").style.display = "block";
 
-    // Disable the submit button to prevent multiple submissions
-    const submitBtn = document.getElementById('submitBtn');
-    submitBtn.style.display = 'none';
+  // Disable the submit button to prevent multiple submissions
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.style.display = "none";
 });
